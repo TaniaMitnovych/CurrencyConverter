c54bee7ba5d4507524fd91aafcd9e22d39050103
TaniaMitnovych <taniamitnovych15@gmail.com>
2023-03-08 18:37:01 +0200

    created CurrencyInput component

---
+++ src/components/CurrencyInput/CurrencyInput.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState } from 'react';
+const CurrencyInput = (props) => {
+  return (
+    <div className='currency-input'>
+      <p className='currency-input__label'>{props.label}</p>
+      <div className='currency-input__input-field'>
+        <input
+          type='number'
+          placeholder={0}
+          value={props.value}
+          onInput={(e) => { props.onChangeAmount(e.target.value) }} />
+        <select
+          value={props.selected}
+          onChange={(e) => { props.onSelectCurrency(e.target.value) }}>
+          {
+            props.currencies.map((currency, i) => {
+              return <option key={i} value={currency}>{currency}</option>
+            })
+          }
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default CurrencyInput;
