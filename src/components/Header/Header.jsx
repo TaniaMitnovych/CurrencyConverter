e6487aeac488b2aa926c2ea27e0e9a4dfbb524f3
TaniaMitnovych <taniamitnovych15@gmail.com>
2023-03-08 12:45:30 +0200

    added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.scss';
+const Header = (props) => {
+  return (
+    <header className='header'>
+      <h1 className='header__title'>Currency converter</h1>
+      <ol className='header__currency-list'>
+        {
+          props.currencies.map((currency,i)=>{
+            return(
+              <li key={i}>
+                <p>{`${currency.amount} ${currency.from} → ${currency.to}`}</p>
+                <p>{i}</p>
+              </li>
+            )
+          })
+        }
+      </ol>
+    </header>
+  );
+};
+
+export default Header;
